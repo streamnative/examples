0236199f0119af88ddf7a927e57c58cca94fbd23
Xiaobing Fang <bingxf@qq.com>
2019-07-09 05:10:32 +0800

    add delayed message example and dead letter topic example (#8)

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ This is a curated list of examples that showcase event streaming using Apache Pu
 ### PubSub
 
 - [Idempotent Producer](pubsub/examples/idempotent-producer.md)
+- [Delayed Message](pubsub/examples/delayed-message.md)
+- [Dead Letter Topic](pubsub/examples/dead-letter-topic.md)
 
 ### Schema
 
---
+++ pubsub/src/main/java/io/streamnative/examples/pubsub/DeadLetterTopicConsumerExample.java
@@ -0,0 +1,92 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamnative.examples.pubsub;
+
+import io.streamnative.examples.common.ConsumerFlags;
+import io.streamnative.examples.common.ExampleRunner;
+import org.apache.pulsar.client.api.*;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Example that demonstrates how to use dead letter topic.
+ **/
+public class DeadLetterTopicConsumerExample extends ExampleRunner<ConsumerFlags> {
+    @Override
+    protected String name() {
+        return DeadLetterTopicConsumerExample.class.getSimpleName();
+    }
+
+    @Override
+    protected String description() {
+        return "Example that demonstrates how to use dead letter topic.";
+    }
+
+    @Override
+    protected ConsumerFlags flags() {
+        return new ConsumerFlags();
+    }
+
+    @Override
+    protected void run(ConsumerFlags flags) throws Exception {
+        try (PulsarClient client = PulsarClient.builder()
+                .serviceUrl(flags.binaryServiceUrl)
+                .build()) {
+
+            int numReceived = 0;
+            try (Consumer<String> consumer = client.newConsumer(Schema.STRING)
+                    .topic(flags.topic)
+                    .subscriptionName(flags.subscriptionName)
+                    .subscriptionType(flags.subscriptionType)
+                    .subscriptionInitialPosition(flags.subscriptionInitialPosition)
+                    .ackTimeout(3, TimeUnit.SECONDS)
+                    .deadLetterPolicy(DeadLetterPolicy.builder()
+                            .maxRedeliverCount(3)
+                            .deadLetterTopic("persistent://public/dlt-example/dlt-example-topic-dlt")
+                            .build())
+                    .subscribe()
+            ) {
+                // consume numMessages messages,
+                // for even numbers, process the message and ack it
+                // for odd numbers, skip ack
+                // when the number of cycles reached redeliverCount times,
+                // DLT will be triggered.
+                while (flags.numMessages <= 0 || numReceived < flags.numMessages) {
+                    Message<String> msg = consumer.receive();
+                    if (Integer.valueOf(msg.getValue().split("-")[1]) % 2 == 0) {
+                        consumer.acknowledge(msg);
+                        System.out.println("Consumer Received message : " + msg.getValue()
+                                + "; Send ack");
+                    } else {
+                        System.out.println("Consumer Received message : " + msg.getValue()
+                                + "; Don't send ack");
+                    }
+                    ++numReceived;
+                }
+            } catch (PulsarClientException ie) {
+                if (ie.getCause() instanceof InterruptedException) {
+                    System.out.println("Successfully received " + numReceived + " messages");
+                    Thread.currentThread().interrupt();
+                } else {
+                    throw ie;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DeadLetterTopicConsumerExample example = new DeadLetterTopicConsumerExample();
+        example.run(args);
+    }
+}
