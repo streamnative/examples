22ae2f897417208dd613ccd92de4b14005b8c859
Sijie Guo <sijie@apache.org>
2020-06-01 17:47:41 -0700

    Add a Github action for flink examples

--- pulsar-flink/src/main/java/io/streamnative/examples/flink/PulsarStreamingWordCount.java
+++ pulsar-flink/src/main/java/io/streamnative/examples/flink/PulsarStreamingWordCount.java
@@ -1,3 +1,16 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.streamnative.examples.flink;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -20,6 +33,9 @@ import org.apache.flink.streaming.connectors.pulsar.FlinkPulsarSink;
 import org.apache.flink.streaming.connectors.pulsar.FlinkPulsarSource;
 import org.apache.flink.streaming.connectors.pulsar.TopicKeyExtractor;
 
+/**
+ * A Flink streaming job that does word counting.
+ */
 public class PulsarStreamingWordCount {
 
     public static void main(String[] args) throws Exception {
@@ -51,8 +67,6 @@ public class PulsarStreamingWordCount {
             new SimpleStringSchema(),
             properties
         ).setStartFromEarliest();
-
-        env.fromCollection()
 
         DataStream<String> stream = env.addSource(source);
         DataStream<WordCount> wc = stream
