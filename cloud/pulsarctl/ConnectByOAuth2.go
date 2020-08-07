94ed5c9780dc769a83bae0f3e16a8dff562fae3b
冉小龙 <rxl@apache.org>
2020-08-07 21:36:36 +0800

    fix pulsarctl examples (#31)
    * fix pulsarctl examples
    
    Signed-off-by: xiaolong.ran <rxl@apache.org>
    
    * update readme
    
    Signed-off-by: xiaolong.ran <rxl@apache.org>
    
    * update readme
    
    Signed-off-by: xiaolong.ran <rxl@apache.org>

--- cloud/pulsarctl/ConnectByOAuth2.go
+++ cloud/pulsarctl/ConnectByOAuth2.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/apache/pulsar-client-go/oauth2"
 	"github.com/streamnative/pulsarctl/pkg/auth"
@@ -9,26 +10,48 @@ import (
 	"log"
 )
 
-func main() {
-	keyFile := "/path/to/keyfile"
+var (
+	help          bool
+	issuerUrl     string
+	audience      string
+	privateKey    string
+	clientId      string
+	webServiceURL string
+)
 
-	pulsarCtlconfig := &common.Config{
-		WebServiceURL:              "https://pulsar.service",
+func main() {
+	flag.Parse()
+
+	if help {
+		flag.Usage()
+	}
+
+	pulsarCtlConfig := &common.Config{
+		WebServiceURL:              webServiceURL,
 		TLSAllowInsecureConnection: true,
 	}
 	issuer := oauth2.Issuer{
-		IssuerEndpoint: "https://oauth2.service",
-		ClientID:       "0Xx..Yyxeny",
-		Audience:       "audience",
+		IssuerEndpoint: issuerUrl,
+		ClientID:       clientId,
+		Audience:       audience,
 	}
-	oauth, err := auth.NewAuthenticationOAuth2WithDefaultFlow(issuer, keyFile)
+	oauth, err := auth.NewAuthenticationOAuth2WithDefaultFlow(issuer, privateKey)
 	if err != nil {
 		log.Fatal(err)
 	}
-	admin := pulsar.NewWithAuthProvider(pulsarCtlconfig, oauth)
+	admin := pulsar.NewWithAuthProvider(pulsarCtlConfig, oauth)
 	ns, err := admin.Namespaces().GetNamespaces("public")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("the namespace is: %s\n", ns)
+}
+
+func init() {
+	flag.StringVar(&issuerUrl, "issuerUrl", "", "issuerUrl is a named external system that provides identity and API access by issuing OAuth access tokens")
+	flag.StringVar(&audience, "audience", "", "audience is the address of the accessed service")
+	flag.StringVar(&privateKey, "privateKey", "", "privateKey is the path of a service account to access your StreamNative Cloud Pulsar cluster")
+	flag.StringVar(&clientId, "clientId", "", "clientId is a public identifier for apps")
+	flag.StringVar(&webServiceURL, "serviceURL", "", "webServiceURL is the address of the accessed HTTP service")
+	flag.BoolVar(&help, "help", false, "help cmd")
 }
