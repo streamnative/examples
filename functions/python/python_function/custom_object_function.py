d114f502af784aa14c52f7fbb48a9271b56676ed
guangning <guangning@apache.org>
2021-04-01 18:09:04 +0800

    Delete no used code

--- functions/python/python_function/custom_object_function.py
+++ functions/python/python_function/custom_object_function.py
@@ -22,25 +22,6 @@
 from pulsar import Function, SerDe
 from python_function.pyserde.serde import MyObject
 
-#class MyObject(object):
-#  def __init__(self):
-#    self.a = 0
-#    self.b = 0
-
-#class CustomSerDe(SerDe):
-#  def __init__(self):
-#    pass
-
-#  def serialize(self, object):
-#    return ("%d,%d" % (object.a, object.b)).encode('utf-8')
-
-#  def deserialize(self, input_bytes):
-#    split = str(input_bytes.decode()).split(',')
-#    retval = MyObject()
-#    retval.a = int(split[0])
-#    retval.b = int(split[1])
-#    return retval
-
 # Function that deals with custom objects
 class CustomObjectFunction(Function):
   def __init__(self):
