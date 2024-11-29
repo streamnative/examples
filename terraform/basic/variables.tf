69d291fb2995fe8c7afecae5d10d631efa4bc4d2
Sijie Guo <sg@streamnative.io>
2024-11-29 13:40:54 -0800

    make it consistent with README

--- terraform/basic/variables.tf
+++ terraform/basic/variables.tf
@@ -4,15 +4,15 @@ variable "org_id" {
 
 variable "instance_name" {
   type = string
-  default = "inst-001"
+  default = "sl-instance"
 }
 
 variable "cluster_name" {
   type = string
-  default = "cluster-001"
+  default = "sl-cluster"
 }
 
 variable "app_name" {
   type = string
-  default = "app-001"
+  default = "sample-app"
 }
