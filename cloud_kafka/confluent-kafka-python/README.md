021e6b291b1018fb74ac78589d8be1b0bd0185c9
Yunze Xu <xyzinfernity@163.com>
2025-03-21 16:03:57 +0800

    (WIP) Add confluent-kafka-python demo

---
+++ cloud_kafka/confluent-kafka-python/README.md
@@ -0,0 +1,13 @@
+# confluent-kafka-python examples
+
+This directory includes examples of Kafka client applications connect to [StreamNative Cloud](https://console.streamnative.cloud).
+
+## Quick Start
+
+Please ensure `poetry` is installed. If not, follow the instructions [here](https://python-poetry.org/docs/#installation).
+
+Install the dependencies:
+
+```bash
+poetry install
+```
---
+++ cloud_kafka/confluent-kafka-python/consumer.py
@@ -0,0 +1,24 @@
+from confluent_kafka import Consumer
+
+c = Consumer({
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'mygroup',
+    'auto.offset.reset': 'earliest'
+})
+
+c.subscribe(['my-topic'])
+
+num_messages = 0
+while num_messages < 10:
+    msg = c.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+
+    print('Received message: {}'.format(msg.value().decode('utf-8')))
+    num_messages += 1
+
+c.close()
