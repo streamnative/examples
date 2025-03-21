e51b15d41762a6dfbd3fc9b8eff4f05fac77ec90
Yunze Xu <xyzinfernity@163.com>
2025-03-21 21:31:20 +0800

    Add consumer example

--- cloud_kafka/confluent-kafka-python/consumer.py
+++ cloud_kafka/confluent-kafka-python/consumer.py
@@ -1,16 +1,41 @@
-from confluent_kafka import Consumer
+from confluent_kafka import Consumer, KafkaError, KafkaException
+import configparser
 
-c = Consumer({
-    'bootstrap.servers': 'localhost:9092',
-    'group.id': 'mygroup',
-    'auto.offset.reset': 'earliest'
+config = configparser.ConfigParser()
+config.read('sncloud.ini')
+
+bootstrap_servers = config['common']['bootstrap.servers']
+topic = config['common']['topic']
+token = config['common']['token']
+group = config['consumer']['group.id']
+print(f'bootstrap_servers: {bootstrap_servers}, topic: {topic}, group: {group}')
+
+def error_cb(err):
+    print("Client error: {}".format(err))
+    if err.code() == KafkaError._ALL_BROKERS_DOWN or \
+            err.code() == KafkaError._AUTHENTICATION:
+        raise KafkaException(err)
+
+consumer = Consumer({
+    'bootstrap.servers': bootstrap_servers,
+    'group.id': group,
+    'auto.offset.reset': 'earliest',
+    'sasl.mechanism': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': 'user',
+    'sasl.password': f'token:{token}',
+    'error_cb': error_cb,
+    'isolation.level': 'read_uncommitted',
 })
 
-c.subscribe(['my-topic'])
+consumer.subscribe([topic])
 
-num_messages = 0
-while num_messages < 10:
-    msg = c.poll(1.0)
+while True:
+    try:
+        msg = consumer.poll(1.0)
+    except KeyboardInterrupt:
+        print('quit')
+        break
 
     if msg is None:
         continue
@@ -19,6 +44,5 @@ while num_messages < 10:
         continue
 
     print('Received message: {}'.format(msg.value().decode('utf-8')))
-    num_messages += 1
 
-c.close()
+consumer.close()
