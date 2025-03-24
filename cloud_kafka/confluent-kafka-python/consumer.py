2c4016fbf56bafda18ac246e1326e7b51487d523
Yunze Xu <xyzinfernity@163.com>
2025-03-24 15:52:39 +0800

    Run pylint for Python scripts

--- cloud_kafka/confluent-kafka-python/consumer.py
+++ cloud_kafka/confluent-kafka-python/consumer.py
@@ -1,48 +1,47 @@
-from confluent_kafka import Consumer, KafkaError, KafkaException
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=too-few-public-methods
 import configparser
+from confluent_kafka import Consumer
 
-config = configparser.ConfigParser()
-config.read('sncloud.ini')
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('sncloud.ini')
 
-bootstrap_servers = config['common']['bootstrap.servers']
-topic = config['common']['topic']
-token = config['common']['token']
-group = config['consumer']['group.id']
-print(f'bootstrap_servers: {bootstrap_servers}, topic: {topic}, group: {group}')
+    bootstrap_servers = config['common']['bootstrap.servers']
+    topic = config['common']['topic']
+    token = config['common']['token']
+    group = config['consumer']['group.id']
+    print(f'bootstrap_servers: {bootstrap_servers}, topic: {topic}, group: {group}')
 
-def error_cb(err):
-    print("Client error: {}".format(err))
-    if err.code() == KafkaError._ALL_BROKERS_DOWN or \
-            err.code() == KafkaError._AUTHENTICATION:
-        raise KafkaException(err)
+    consumer = Consumer({
+        'bootstrap.servers': bootstrap_servers,
+        'group.id': group,
+        'auto.offset.reset': 'earliest',
+        'sasl.mechanism': 'PLAIN',
+        'security.protocol': 'SASL_SSL',
+        'sasl.username': 'user',
+        'sasl.password': f'token:{token}',
+        'error_cb': lambda err: print(f'Error: {err}'),
+        'isolation.level': 'read_uncommitted',
+    })
 
-consumer = Consumer({
-    'bootstrap.servers': bootstrap_servers,
-    'group.id': group,
-    'auto.offset.reset': 'earliest',
-    'sasl.mechanism': 'PLAIN',
-    'security.protocol': 'SASL_SSL',
-    'sasl.username': 'user',
-    'sasl.password': f'token:{token}',
-    'error_cb': error_cb,
-    'isolation.level': 'read_uncommitted',
-})
+    consumer.subscribe([topic])
 
-consumer.subscribe([topic])
+    while True:
+        try:
+            msg = consumer.poll(1.0)
+        except KeyboardInterrupt:
+            print('quit')
+            break
 
-while True:
-    try:
-        msg = consumer.poll(1.0)
-    except KeyboardInterrupt:
-        print('quit')
-        break
+        if msg is None:
+            continue
+        if msg.error():
+            print(f'Consumer error: {msg.error()}')
+            continue
 
-    if msg is None:
-        continue
-    if msg.error():
-        print("Consumer error: {}".format(msg.error()))
-        continue
+        print(f'Received message: {msg.value().decode("utf-8")}')
 
-    print('Received message: {}'.format(msg.value().decode('utf-8')))
-
-consumer.close()
+    consumer.close()
--- cloud_kafka/confluent-kafka-python/producer.py
+++ cloud_kafka/confluent-kafka-python/producer.py
@@ -1,30 +1,37 @@
-from confluent_kafka import Producer
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=too-few-public-methods
 import configparser
+from confluent_kafka import Producer
 
-config = configparser.ConfigParser()
-config.read('sncloud.ini')
-
-bootstrap_servers = config['common']['bootstrap.servers']
-topic = config['common']['topic']
-token = config['common']['token']
-print(f'bootstrap_servers: {bootstrap_servers}, topic: {topic}')
-
-producer = Producer({
-    'bootstrap.servers': bootstrap_servers,
-    'sasl.mechanism': 'PLAIN',
-    'security.protocol': 'SASL_SSL',
-    'sasl.username': 'user',
-    'sasl.password': f'token:{token}',
-})
-
-def acked(err, msg):
+def on_delivery(err, msg):
     if err is not None:
-        print('Failed to deliver message: {}'.format(err.str()))
+        print(f'Failed to deliver message: {err.str()}')
     else:
-        print('Produced to: {} [{}] @ {}'.format(msg.topic(), msg.partition(), msg.offset()))
+        print(f'Produced to: {msg.topic()} [{msg.partition()}] @ {msg.offset()}')
 
-for i in range(10):
-    producer.poll(0)
-    producer.produce(topic, value=f'msg-{i}', callback=acked)
 
-producer.poll()
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('sncloud.ini')
+
+    bootstrap_servers = config['common']['bootstrap.servers']
+    topic = config['common']['topic']
+    token = config['common']['token']
+    print(f'bootstrap_servers: {bootstrap_servers}, topic: {topic}')
+
+    producer = Producer({
+        'bootstrap.servers': bootstrap_servers,
+        'sasl.mechanism': 'PLAIN',
+        'security.protocol': 'SASL_SSL',
+        'sasl.username': 'user',
+        'sasl.password': f'token:{token}',
+    })
+
+
+    for i in range(10):
+        producer.poll(0)
+        producer.produce(topic, value=f'msg-{i}', callback=on_delivery)
+
+    producer.poll()
