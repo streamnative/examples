9ca1debfd39bac5af324d78037ffcfb3b18298d4
Yunze Xu <xyzinfernity@163.com>
2025-04-14 16:50:12 +0800

    Switch to simpleini because of the line length limit bug of inih

--- cloud_kafka/librdkafka/config.h
+++ cloud_kafka/librdkafka/config.h
@@ -1,12 +1,17 @@
 #pragma once
 
-#include <INIReader.h>
-
 #include <stdexcept>
+
+#include "SimpleIni.h"
 
 class Config {
  public:
-  Config(const std::string& filename) : reader_(filename) {}
+  Config(const std::string& filename) {
+    auto rc = ini_.LoadFile(filename.c_str());
+    if (rc < 0) {
+      throw std::runtime_error("Failed to load config file: " + filename);
+    }
+  }
 
   std::string bootstrap_servers() const {
     return get_non_empty_string("common", "bootstrap.servers");
@@ -14,22 +19,22 @@ class Config {
 
   std::string topic() const { return get_non_empty_string("common", "topic"); }
 
-  std::string token() const { return reader_.GetString("common", "token", ""); }
+  std::string token() const { return ini_.GetValue("common", "token", ""); }
 
   std::string schema_registry_url() const {
     return get_non_empty_string("schema.registry", "url");
   }
 
   std::string group_id() const {
-    return reader_.GetString("consumer", "group.id", "default-group");
+    return ini_.GetValue("consumer", "group.id", "default-group");
   }
 
  private:
-  INIReader reader_;
+  CSimpleIniA ini_;
 
   std::string get_non_empty_string(const std::string& section,
                                    const std::string& name) const {
-    auto result = reader_.GetString(section, name, "");
+    std::string result = ini_.GetValue(section.c_str(), name.c_str(), "");
     if (result.empty()) {
       throw std::runtime_error("empty " + name + " in [" + section +
                                "] section");
